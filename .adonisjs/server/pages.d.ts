0e10c0bc03b2b4ea21c3f450f2b234c689ed72a1
engineermuzamil <muhammadmuzamilhussain5@gmail.com>
2026-04-24 17:59:58 +0500

    feat: add shared-note, flash-toast, and trash-section to InertiaPages

--- .adonisjs/server/pages.d.ts
+++ .adonisjs/server/pages.d.ts
@@ -17,10 +17,13 @@ declare module '@adonisjs/inertia/types' {
     'errors/not_found': ExtractProps<(typeof import('../../inertia/pages/errors/not_found.tsx'))['default']>
     'errors/server_error': ExtractProps<(typeof import('../../inertia/pages/errors/server_error.tsx'))['default']>
     'home': ExtractProps<(typeof import('../../inertia/pages/home.tsx'))['default']>
+    'notes/flash-toast': ExtractProps<(typeof import('../../inertia/pages/notes/flash-toast.tsx'))['default']>
     'notes/index': ExtractProps<(typeof import('../../inertia/pages/notes/index.tsx'))['default']>
     'notes/note-card': ExtractProps<(typeof import('../../inertia/pages/notes/note-card.tsx'))['default']>
     'notes/note-form': ExtractProps<(typeof import('../../inertia/pages/notes/note-form.tsx'))['default']>
+    'notes/shared-note': ExtractProps<(typeof import('../../inertia/pages/notes/shared-note.tsx'))['default']>
     'notes/sort-selector': ExtractProps<(typeof import('../../inertia/pages/notes/sort-selector.tsx'))['default']>
+    'notes/trash-section': ExtractProps<(typeof import('../../inertia/pages/notes/trash-section.tsx'))['default']>
     'notes/view-switcher': ExtractProps<(typeof import('../../inertia/pages/notes/view-switcher.tsx'))['default']>
     'projects/index': ExtractProps<(typeof import('../../inertia/pages/projects/index.tsx'))['default']>
     'projects/project-card': ExtractProps<(typeof import('../../inertia/pages/projects/project-card.tsx'))['default']>
@@ -30,8 +33,5 @@ declare module '@adonisjs/inertia/types' {
     'todos/todo-card': ExtractProps<(typeof import('../../inertia/pages/todos/todo-card.tsx'))['default']>
     'todos/todo-form': ExtractProps<(typeof import('../../inertia/pages/todos/todo-form.tsx'))['default']>
     'todos/view-switcher': ExtractProps<(typeof import('../../inertia/pages/todos/view-switcher.tsx'))['default']>
-    'notes/shared-note': ExtractProps<(typeof import('../../inertia/pages/notes/shared-note.tsx'))['default']>
-    'notes/flash-toast': ExtractProps<(typeof import('../../inertia/pages/notes/flash-toast.tsx'))['default']>
-    'notes/trash-section': ExtractProps<(typeof import('../../inertia/pages/notes/trash-section.tsx'))['default']>
   }
 }
